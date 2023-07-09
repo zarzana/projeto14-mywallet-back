acf29b77f9b55b15d3e69f43a3d4179897c369cc
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-07-09 17:20:53 -0300

    Fixed /signup not validating properly.

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { signUp, signIn } from '../controllers/authController.js';
-import { signUpValidator } from '../validators/authValidators.js';
+import { signInValidator, signUpValidator } from '../validators/authValidators.js';
 
 const router = Router();
 
 router.post('/signup', signUpValidator, signUp);
-router.post('/signin', signIn);
+router.post('/signin', signInValidator, signIn);
 
 export default router;
