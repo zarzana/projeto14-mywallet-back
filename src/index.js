4e2302e4c169d96e79e5e92e5727981e3d752b59
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-07-10 14:02:38 -0300

    Code adjusted for deploy on Render.

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+})
